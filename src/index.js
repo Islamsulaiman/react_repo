910b55f4cde2002e92c519ef5a0969f701c3419a
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-11 23:23:22 +0300

    Using props to make differant components from the base component

--- src/index.js
+++ src/index.js
@@ -2,24 +2,40 @@ import React from "react"
 import ReactDom from "react-dom"
 import "./index.css";
 
-// variables
-let img = "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg";
-let price = "$ 12.13";
-let author = "World chicken";
+let firstBook = {
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg",
+    price: "$ 12.13",
+    author: "World chicken"
+}
+
+let secondBook = {
+    img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
+    price: "$ 10.15",
+    author: "Robert kawasaki"
+}
 
 function BookList() {
     return (
         <section className="list">
-            <Book />
+            <Book 
+                image = {firstBook.img}
+                prices = {firstBook.price}
+                authors = {firstBook.author} 
+            />
+            <Book 
+                image = {secondBook.img}
+                prices = {secondBook.price}
+                authors = {secondBook.author} 
+            />
         </section>
     );
 }
 
-const Book = () => {
+const Book = (props) => {
     return (<article className="book">
-        <img src={img} alt="Books" />
-        <h4>{price}</h4>
-        <h4>{author}</h4>
+        <img src={props.image} alt="Books" />
+        <h4>{props.prices}</h4>
+        <h4>{props.authors}</h4>
     </article>);
 }
 
