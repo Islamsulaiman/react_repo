deb8a0caad5f838ec8dd10cb3f04223d2798c11a
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-11 22:37:12 +0300

    from nested components to one component

--- src/index.js
+++ src/index.js
@@ -7,31 +7,16 @@ function BookList() {
     return (
         <section className="list">
             <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
         </section>
     );
 }
 
 const Book = () => {
-    return <article className="book">
-        <Image />
-        <Price />
-        <Author />
-    </article>
+    return (<article className="book">
+        <img src="https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg" alt="Books" />
+        <h4><sup>$</sup>12 <sup>13</sup></h4>
+        <h4>World chicken</h4>
+    </article>);
 }
 
-const Image = () => {
-    return <img src="https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg" alt="Books" />
-}
-
-const Price = () => {
-    return <h4><sup>$</sup>12 <sup>13</sup></h4>
-}
-const Author = () => {
-    return <h4>World chicken</h4>
-}
 ReactDom.render(<BookList />, document.getElementById("root"));
