ca740b221796f615e86211b09d291793f63ef04f
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-11 11:36:24 +0300

    Creating first nested component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,19 @@ import React from "react"
 import ReactDom from "react-dom"
 
 function Greeting() {
-    return <h4>This is my first component</h4>;
+    return (
+        <div>
+            <Name />
+            <Massage />
+        </div>
+    )
+}
+
+let Name = () => {
+    return <h2>Islam sulaiman</h2>
+}
+let Massage = () => {
+    return <p>Iam Islam, I want to learn React</p>
 }
 
 ReactDom.render(<Greeting />, document.getElementById("root"));
