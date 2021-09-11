dc7d8088a20598d96be8ee8d8b23356d5c330cf8
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-11 22:44:45 +0300

    adding JS variable to HTML elements enstead of hardcoding value to HTML

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react"
 import ReactDom from "react-dom"
 import "./index.css";
 
+// variables
+let img = "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg";
+let price = "$ 12.13";
+let author = "World chicken";
 
 function BookList() {
     return (
@@ -13,9 +17,9 @@ function BookList() {
 
 const Book = () => {
     return (<article className="book">
-        <img src="https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg" alt="Books" />
-        <h4><sup>$</sup>12 <sup>13</sup></h4>
-        <h4>World chicken</h4>
+        <img src={img} alt="Books" />
+        <h4>{price}</h4>
+        <h4>{author}</h4>
     </article>);
 }
 
