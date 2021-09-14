3271221d364f7fb9af7b7cb46f6c706f5fc8e329
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-14 11:16:35 +0300

    Create a list of objects to use it in differant component, to avoid refferancing every property of the objectsinside the main component

--- src/index.js
+++ src/index.js
@@ -2,42 +2,36 @@ import React from "react"
 import ReactDom from "react-dom"
 import "./index.css";
 
-let firstBook = {
-    img: "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg",
-    price: "$ 12.13",
-    author: "World chicken"
-}
 
-let secondBook = {
-    img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
-    price: "$ 10.15",
-    author: "Robert kawasaki"
-}
+let bookArray = [
+    {
+        img: "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg",
+        price: "$ 12.13",
+        author: "World chicken"
+    },
+    {
+        img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
+        price: "$ 10.15",
+        author: "Robert kawasaki"
+    }
+
+];
+
 function BookList() {
     return (
         <section className="list">
-            <Book 
-                image = {firstBook.img}
-                prices = {firstBook.price}
-                authors = {firstBook.author} 
-            >
-                <p>This is a children for this specific component.</p>
-            </Book>
-            <Book 
-                image = {secondBook.img}
-                prices = {secondBook.price}
-                authors = {secondBook.author} 
-            />
+            {bookArray.map((item) => {
+                return <Book book = {item}/>
+            })}
         </section>
     );
 }
 const Book = (props) => {
-    let {image, prices, authors, children} = props;
-    return (<article className="book">
-        <img src={image} alt="Books" />
-        <h4>{prices}</h4>
-        <h4>{authors}</h4>
-        <h4>{children}</h4>
+    let {img, price, author} = props.book;
+    return (<article className = "book">
+        <img src={img} alt="Books" />
+        <h4>{price}</h4>
+        <h4>{author}</h4>
     </article>);
 }
 
