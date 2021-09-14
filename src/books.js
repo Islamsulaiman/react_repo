a269e1923af2677c3d7a4c83591aab76d9ca81fc
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-14 18:45:56 +0300

    Seperate the objects array from index.js to external file book.js, using import and export

---
+++ src/books.js
@@ -0,0 +1,24 @@
+// This file contains the array of object used in creating different component from the same origin component
+
+
+export const bookArray = [
+    {
+        id:1,
+        img: "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg",
+        price: "$ 12.13",
+        author: "World chicken"
+    },
+    {
+        id: 2,
+        img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
+        price: "$ 10.15",
+        author: "Robert kawasaki"
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/71dAc-6oWNL._AC_UL480_FMwebp_QL65_.jpg",
+        price: "$ 49.15",
+        author: "E. Lockhart"
+    }
+
+];
--- src/index.js
+++ src/index.js
@@ -2,28 +2,8 @@ import React from "react"
 import ReactDom from "react-dom"
 import "./index.css";
 
-
-let bookArray = [
-    {
-        id:1,
-        img: "https://images-na.ssl-images-amazon.com/images/I/51ddcXBCmcS._SY498_BO1,204,203,200_.jpg",
-        price: "$ 12.13",
-        author: "World chicken"
-    },
-    {
-        id: 2,
-        img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
-        price: "$ 10.15",
-        author: "Robert kawasaki"
-    },
-    {
-        id: 3,
-        img: "https://m.media-amazon.com/images/I/71dAc-6oWNL._AC_UL480_FMwebp_QL65_.jpg",
-        price: "$ 49.15",
-        author: "E. Lockhart"
-    }
-
-];
+//import the array of objects
+import {bookArray} from './books'
 
 function BookList() {
     return (
