e861d04cbd80605b3f2ab111f470cb5ea746a85b
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-14 15:25:15 +0300

    Added a function with an argument to an event, should be added as a calback function not an argument to the event

--- src/index.js
+++ src/index.js
@@ -36,12 +36,17 @@ function BookList() {
 }
 const Book = (props) => {
     let {img, price, author} = props;
+
+    let complexFunc = (price) => console.log(price);
+
     let logAuthor = ()=> console.log(author);
+
     return (<article className = "book">
         <img src={img} alt="Books"  onMouseOver ={() => console.log("Hover")} />
         <h4>{price}</h4>
         <h4>{author}</h4>
         <button type="button" onClick={logAuthor}>Press me</button>
+        <button type="button" onClick={() => complexFunc(price)}>complex</button>
     </article>);
 }
 
