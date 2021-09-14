35033a2197308ba19d0873e1c6fd15f178e06c96
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-14 14:21:35 +0300

    Create button with onClick event in the sup-component

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ let bookArray = [
         img: "https://m.media-amazon.com/images/I/81dQwQlmAXL._AC_UL480_FMwebp_QL65_.jpg",
         price: "$ 10.15",
         author: "Robert kawasaki"
+    },
+    {
+        id: 3,
+        img: "https://m.media-amazon.com/images/I/71dAc-6oWNL._AC_UL480_FMwebp_QL65_.jpg",
+        price: "$ 49.15",
+        author: "E. Lockhart"
     }
 
 ];
@@ -30,10 +36,12 @@ function BookList() {
 }
 const Book = (props) => {
     let {img, price, author} = props;
+    let logAuthor = ()=> console.log(author);
     return (<article className = "book">
         <img src={img} alt="Books" />
         <h4>{price}</h4>
         <h4>{author}</h4>
+        <button type="button" onClick={logAuthor}>Press me</button>
     </article>);
 }
 
